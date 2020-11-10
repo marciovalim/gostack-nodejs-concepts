65cf713aefff7dabb81ce205c2b0d17495bafa00
marciovalim <marciogsvalim@gmail.com>
2020-11-10 12:40:57 -0300

    🚀 challenge completed

--- src/app.js
+++ src/app.js
@@ -1,33 +1,72 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: createId, validate: isIdValid } = require("uuid");
 
 const app = express();
+const repositories = [];
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+  if (id == undefined || !isIdValid(id)) {
+    return response.status(400).json({ error: "Id is incorrect" });
+  }
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  const idNotExist = repositoryIndex < 0;
+  if (idNotExist) {
+    return response.status(400).json({ error: "Id not found" });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+  return next();
+}
 
 app.use(express.json());
 app.use(cors());
+app.use("/repositories/:id", validateId);
 
-const repositories = [];
-
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (_, response) => {
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = _repositoryFactory(title, url, techs);
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { repositoryIndex } = request;
+
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { repositoryIndex } = request;
+  repositories.splice(repositoryIndex);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { repositoryIndex } = request;
+  const repository = repositories[repositoryIndex];
+  repository.likes++;
+  return response.json(repository);
 });
+
+function _repositoryFactory(title, url, techs) {
+  return {
+    id: createId(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0,
+  };
+}
 
 module.exports = app;
